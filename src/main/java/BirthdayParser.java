5990214f21f94604580372131eb5e1fd4bc66016
catarata02 <catarata02@hotmail.com>
2024-11-06 11:05:25 +0100

    remove kotlin code

--- src/main/java/BirthdayParser.java
+++ src/main/java/BirthdayParser.java
@@ -2,12 +2,8 @@ import java.util.Collections;
 import java.util.List;
 
 public class BirthdayParser {
-    public String say() {
-        return "World";
-    }
-
     public List<Birthday> parse(String fileContent) {
-        if(fileContent.isBlank())
+        if (fileContent.isBlank())
             return Collections.emptyList();
         return null;
     }
