d9da32fe1b86f0960cba9a317cb1edaab648d0da
catarata02 <catarata02@hotmail.com>
2024-12-04 11:09:49 +0100

    test case: aged brie use cases

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -17,6 +17,39 @@ internal class GildedRoseTest {
         assertEquals(expectedAfterUpdate, app.items[0])
     }
 
+    @Test
+    fun `GIVEN aged brie with positive sellIn and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 1`() {
+        val expectedAfterUpdate = Item(name = "Aged Brie", sellIn = 9, quality = 41)
+        val items = listOf(Item("Aged Brie", 10, 40))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(expectedAfterUpdate, app.items[0])
+    }
+
+    @Test
+    fun `GIVEN aged brie with positive sellIn and quality 50 WHEN updateQuality THEN sellIn is decreased and quality stays the same`() {
+        val expectedAfterUpdate = Item(name = "Aged Brie", sellIn = 9, quality = 50)
+        val items = listOf(Item("Aged Brie", 10, 50))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(expectedAfterUpdate, app.items[0])
+    }
+
+    @Test
+    fun `GIVEN aged brie with negative sellIn and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 2`() {
+        val expectedAfterUpdate = Item(name = "Aged Brie", sellIn = -2, quality = 42)
+        val items = listOf(Item("Aged Brie", -1, 40))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(expectedAfterUpdate, app.items[0])
+    }
+
 }
 
 
