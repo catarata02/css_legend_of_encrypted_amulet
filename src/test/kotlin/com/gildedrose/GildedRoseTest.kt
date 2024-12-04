8608750e52de4440044c1de6c29ae3fd88c4e1d7
catarata02 <catarata02@hotmail.com>
2024-12-04 11:32:36 +0100

    test case: extracting creation of items to methods

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -2,25 +2,24 @@ package com.gildedrose
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import kotlin.math.exp
 
 internal class GildedRoseTest {
-
+    
     @Test
     fun `GIVEN normal item with positive sellIn and quality WHEN updateQuality THEN sellIn and quality are decreased by 1`() {
-        val expectedAfterUpdate = Item(name = "foo", sellIn = 9, quality = 49)
-        val items = listOf(Item("foo", 10, 50))
+        val expectedAfterUpdate = createNormalItem(9, 49)
+        val items = listOf(createNormalItem(10, 50))
         val app = GildedRose(items)
-        
+
         app.updateQuality()
-        
+
         assertEquals(expectedAfterUpdate, app.items[0])
     }
 
     @Test
     fun `GIVEN aged brie with positive sellIn and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 1`() {
-        val expectedAfterUpdate = Item(name = "Aged Brie", sellIn = 9, quality = 41)
-        val items = listOf(Item("Aged Brie", 10, 40))
+        val expectedAfterUpdate = createAgedBrie(9, 41)
+        val items = listOf(createAgedBrie(10, 40))
         val app = GildedRose(items)
 
         app.updateQuality()
@@ -30,8 +29,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `GIVEN aged brie with positive sellIn and quality 50 WHEN updateQuality THEN sellIn is decreased and quality stays the same`() {
-        val expectedAfterUpdate = Item(name = "Aged Brie", sellIn = 9, quality = 50)
-        val items = listOf(Item("Aged Brie", 10, 50))
+        val expectedAfterUpdate = createAgedBrie(9, 50)
+        val items = listOf(createAgedBrie(10, 50))
         val app = GildedRose(items)
 
         app.updateQuality()
@@ -41,8 +40,8 @@ internal class GildedRoseTest {
 
     @Test
     fun `GIVEN aged brie with negative sellIn and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 2`() {
-        val expectedAfterUpdate = Item(name = "Aged Brie", sellIn = -2, quality = 42)
-        val items = listOf(Item("Aged Brie", -1, 40))
+        val expectedAfterUpdate = createAgedBrie(-2, 42)
+        val items = listOf(createAgedBrie(-1, 40))
         val app = GildedRose(items)
 
         app.updateQuality()
@@ -52,28 +51,30 @@ internal class GildedRoseTest {
 
     @Test
     fun `GIVEN Backstage passes to a TAFKAL80ETC concert with sellIn above 11 and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 1`() {
-        val expectedAfterUpdate = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 15, quality = 42)
-        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 16, 41))
+        val expectedAfterUpdate = createBackstageItem(sellIn = 15, quality = 42)
+        val items = listOf(createBackstageItem( 16, 41))
         val app = GildedRose(items)
 
         app.updateQuality()
 
         assertEquals(expectedAfterUpdate, app.items[0])
     }
-@Test
+
+    @Test
     fun `GIVEN Backstage passes to a TAFKAL80ETC concert with sellIn above 6 but under 11 and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 2`() {
-        val expectedAfterUpdate = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 9, quality = 43)
-        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 41))
+        val expectedAfterUpdate = createBackstageItem(9, 43)
+        val items = listOf(createBackstageItem(10, 41))
         val app = GildedRose(items)
 
         app.updateQuality()
 
         assertEquals(expectedAfterUpdate, app.items[0])
     }
+
     @Test
     fun `GIVEN Backstage passes to a TAFKAL80ETC concert with sellIn below 6 and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 3`() {
-        val expectedAfterUpdate = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 4, quality = 44)
-        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 41))
+        val expectedAfterUpdate = createBackstageItem(sellIn = 4, quality = 44)
+        val items = listOf(createBackstageItem(5, 41))
         val app = GildedRose(items)
 
         app.updateQuality()
@@ -83,14 +84,29 @@ internal class GildedRoseTest {
 
     @Test
     fun `GIVEN Backstage passes to a TAFKAL80ETC concert with sellIn below 0 and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is 0`() {
-        val expectedAfterUpdate = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = -2, quality = 0)
-        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", -1, 41))
+        val expectedAfterUpdate = createBackstageItem(sellIn = -2, quality = 0)
+        val items = listOf(createBackstageItem(-1, 41))
         val app = GildedRose(items)
 
         app.updateQuality()
 
         assertEquals(expectedAfterUpdate, app.items[0])
     }
+
+    private fun createNormalItem(
+        sellIn: Int,
+        quality: Int,
+    ) = Item("foo", sellIn, quality)
+
+    private fun createAgedBrie(
+        sellIn: Int,
+        quality: Int,
+    ) = Item("Aged Brie", sellIn, quality)
+
+    private fun createBackstageItem(
+        sellIn: Int,
+        quality: Int,
+    ) = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = sellIn, quality = quality)
 }
 
 
