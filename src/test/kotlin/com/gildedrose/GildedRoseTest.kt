08c9d61a9ca337b82140d639a55de88a8fc664a4
catarata02 <catarata02@hotmail.com>
2024-12-04 11:25:08 +0100

    test case: backstage passes use cases

--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -50,6 +50,47 @@ internal class GildedRoseTest {
         assertEquals(expectedAfterUpdate, app.items[0])
     }
 
+    @Test
+    fun `GIVEN Backstage passes to a TAFKAL80ETC concert with sellIn above 11 and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 1`() {
+        val expectedAfterUpdate = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 15, quality = 42)
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 16, 41))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(expectedAfterUpdate, app.items[0])
+    }
+@Test
+    fun `GIVEN Backstage passes to a TAFKAL80ETC concert with sellIn above 6 but under 11 and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 2`() {
+        val expectedAfterUpdate = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 9, quality = 43)
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 10, 41))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(expectedAfterUpdate, app.items[0])
+    }
+    @Test
+    fun `GIVEN Backstage passes to a TAFKAL80ETC concert with sellIn below 6 and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is increased by 3`() {
+        val expectedAfterUpdate = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = 4, quality = 44)
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", 5, 41))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(expectedAfterUpdate, app.items[0])
+    }
+
+    @Test
+    fun `GIVEN Backstage passes to a TAFKAL80ETC concert with sellIn below 0 and quality less than 50 WHEN updateQuality THEN sellIn is decreased and quality is 0`() {
+        val expectedAfterUpdate = Item(name = "Backstage passes to a TAFKAL80ETC concert", sellIn = -2, quality = 0)
+        val items = listOf(Item("Backstage passes to a TAFKAL80ETC concert", -1, 41))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(expectedAfterUpdate, app.items[0])
+    }
 }
 
 
